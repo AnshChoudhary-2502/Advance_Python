690bd760ef9f60093cb94c3bbc3a72a4d0da7718
AnshChoudhary-2502 <ansh.choudhary@innvonix.in>
2026-01-16 11:06:08 +0530

    file_handling

---
+++ exception.py
@@ -0,0 +1,14 @@
+def file_handling(file_name):
+    try:
+        # with open(file_name, "a") as file:
+        #     # content = file.read()
+        #     # print(content)
+        #     file.write("My age is 22")
+        with open(file_name, "r") as file:
+            content = file.read()
+            print(content)    
+
+    except FileNotFoundError:
+        print("File does not exist.")
+
+file_handling("hello.txt")
